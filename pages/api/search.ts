1832ae1b6802b514087fb19c414aa3feaafe6cf7
apirut <apirut@merefine.io>
2024-02-20 10:07:05 +0700

    fix/Cors by move API

---
+++ pages/api/search.ts
@@ -0,0 +1,16 @@
+import axios from "axios";
+
+export default async function handler(req, res) {
+  const { q, type, page, region, fields } = req.query;
+  try {
+    const response = await axios.get(`https://invidious.fdn.fr/api/v1/search`, {
+      params: { q, type, page, region, fields },
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+      },
+    });
+    res.status(200).json(response.data);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+}
