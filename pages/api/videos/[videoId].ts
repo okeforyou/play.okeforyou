afd41aca39360dd0daa64bb2e15cc474e479ce05
apirut <apirut@merefine.io>
2024-08-26 14:05:51 +0700

    fix/change web api

--- pages/api/videos/[videoId].ts
+++ pages/api/videos/[videoId].ts
@@ -5,7 +5,7 @@ export default async function handler(req, res) {
 
   try {
     const response = await axios.get(
-      `http://iv.ggtyler.dev/api/v1/videos/${videoId}`,
+      `https://iv.ggtyler.dev/api/v1/videos/${videoId}`,
       {
         params: {
           fields: "recommendedVideos",
