8be5780882151ce33d843f7d2833963d6396de32
apirut <apirut@merefine.io>
2025-03-06 10:58:38 +0700

    set Show ads every end

--- components/YoutubePlayer.tsx
+++ components/YoutubePlayer.tsx
@@ -225,7 +225,7 @@ function YoutubePlayer({
   };
 
   useEffect(() => {
-    if (!isLogin && videoCount % 3 == 0 && videoCount !== 0) {
+    if (!isLogin && videoCount % 1 == 0 && videoCount !== 0) {
       handlePause();
       setIsShowAds(false);
       setTimeout(() => setIsShowAds(true), 200);
